88b4bf1d142e18d718738d85a85042c49dc98195
Adam Shrout <geezer85@users.noreply.github.com>
2021-03-08 11:24:40 -0600

    Add build.sh, example configs, and set up submodules.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # libretech-cc-uboot-build
 Scripts for building mainline u-boot images on Libretech CC (Le Potato)
+
+Notes:
+
+The closed-source fip_create and aml_encrypt_gxl binaries from the chipset vendor are compiled (statically) for x86_64 Linux. It is not currently possible to build the vendor u-boot on any other OS or architecture.
+
+Steps:
+
+1) Download or build cross-compile toolchains for arm-none-eabi- and aarch64-none-elf- targets and add them to your PATH:
+
+The easiest option is to use the ARM Developer toolchains from here: https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-a/downloads. Download both AArch32 bare metal target and AArch64 ELF bare-metal target compilers, extract them, and add the bin directory from each to your PATH.
+
+2) Edit or remove config (optional):
+
+Two example mainline u-boot configurations are included. The config symlink points to config.emmc. If you use an SD card instead you can point it to config.sdcard. If you would prefer defaults simply delete the symlink before proceeding. You can also edit it like any other KConfig file (make menuconfig, etc.) if you copy it to the mainline-uboot directory first. If it does not exist run "git submodule update --remote --recursive" in the top-level directory.
+
+3) Run build.sh
+
+4) Install on eMMC/SD card:
+
+Warning: Make sure to use the correct devices or you'll wipe your OS. If in doubt Balena Etcher works for the SD card.
+
+For SD Card:
+# dd if=fip/u-boot.bin.sd.bin of=/dev/xxx conv=fsync,notrunc bs=1 count=444
+# dd if=fip/u-boot.bin.sd.bin of=/dev/xxx conv=fsync,notrunc bs=512 skip=1 seek=1
+
+For eMMC:
+
+You will need to do this from an OS running on the device. The debian/ubuntu images from libre.computer or Armbian on an SD card work.
+
+# dd if=u-boot.bin of=/dev/xxx bs=512 seek=1
---
+++ build.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+# Pull in vendor and mainline u-boot
+git submodule update --remote --recursive
+
+# Check for cross-compile support
+which arm-none-eabi-gcc 2>&1 >/dev/null
+if [ $? -ne 0 ]; then
+	echo "Missing \"arm-none-eabi-\" cross-compiler. See Readme.md for details."
+	exit 1
+fi
+which aarch64-none-elf-gcc 2>&1 >/dev/null
+if [ $? -ne 0 ]; then
+	echo "Missing \"aarch64-none-elf-\" cross-compiler. See Readme.md for details."
+	exit 2
+fi
+
+# Set Environment variables for cross-compile
+export ARCH=arm
+export CROSS_COMPILE=aarch64-none-elf-
+
+# Build fip blobs needed to sigm uboot image
+
+echo "Building fip.bin from vendor u-boot. This will take a few minutes..."
+cd vendor-uboot
+make libretech_cc_defconfig 2>&1 >../build.log
+make -j fip.bin 2>&1 >>../build.log
+if [ $? -ne 0 ]; then
+	echo "Build failed! Check build.log."
+	exit 3
+else
+	echo "Success!"
+	cd ..
+fi
+
+# If config exists copy it first, else use defaults.
+if [ -x config ]; then
+	cp config mainline-uboot/.config && cd mainline-uboot
+else
+	cd mainline-uboot && make libretech-cc_defconfig 2>&1 >>../build.log
+fi
+
+# Build mainline-uboot
+echo "Building mainline u-boot. This will take a few minutes..."
+make -j 2>&1 >>../build.log
+if [ $? -ne 0 ]; then
+	echo "Build failed! Check build.log"
+	exit 4
+else
+	echo "Success!"
+	cd ..
+fi
+
+# Sign boot image
+export FIPDIR=vendor-uboot/fip
+mkdir fip
+cp $FIPDIR/gxl/bl2.bin fip/
+cp $FIPDIR/gxl/acs.bin fip/
+cp $FIPDIR/gxl/bl21.bin fip/
+cp $FIPDIR/gxl/bl30.bin fip/
+cp $FIPDIR/gxl/bl301.bin fip/
+cp $FIPDIR/gxl/bl31.img fip/
+cp mainline-uboot/u-boot.bin fip/bl33.bin
+$FIPDIR/blx_fix.sh fip/bl30.bin fip/zero_tmp fip/bl30_zero.bin fip/bl301.bin fip/bl301_zero.bin fip/bl30_new.bin bl30
+$FIPDIR/acs_tool.pyc fip/bl2.bin fip/bl2_acs.bin fip/acs.bin 0
+$FIPDIR/blx_fix.sh fip/bl2_acs.bin fip/zero_tmp fip/bl2_zero.bin fip/bl21.bin fip/bl21_zero.bin fip/bl2_new.bin bl2
+$FIPDIR/gxl/aml_encrypt_gxl --bl3enc --input fip/bl30_new.bin
+$FIPDIR/gxl/aml_encrypt_gxl --bl3enc --input fip/bl31.img
+$FIPDIR/gxl/aml_encrypt_gxl --bl3enc --input fip/bl33.bin
+$FIPDIR/gxl/aml_encrypt_gxl --bl2sig --input fip/bl2_new.bin --output fip/bl2.n.bin.sig
+$FIPDIR/gxl/aml_encrypt_gxl --bootmk --output fip/u-boot.bin --bl2 fip/bl2.n.bin.sig --bl30 fip/bl30_new.bin.enc --bl31 fip/bl31.img.enc --bl33 fip/bl33.bin.enc
+cp fip/u-boot.bin .
+cp fip/u-boot.bin.sd.bin .
+echo "Unless there was an error u-boot.bin (eMMC) and u-boot.bin.sd.bin (SD card) should be ready."
+
+exit 0
